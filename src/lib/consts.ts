7ae1fe38fda4c1c1d7f6376c74b0dec5868b09b2
Franco Carreras <fcarreras777@gmail.com>
2025-11-12 22:51:05 -0300

    add social networks constants with icons and links

---
+++ src/lib/consts.ts
@@ -0,0 +1,13 @@
+import Github from "@assets/icons/Github.astro";
+import LinkedIn from "@assets/icons/LinkedIn.astro";
+import Mail from "@assets/icons/Mail.astro";
+
+export const networks = [
+  { label: "GitHub Profil", href: "https://github.com/Fr4nco77", icon: Github },
+  {
+    label: "LinkedIn Profil",
+    href: "https://www.linkedin.com/in/franco-carreras-b7a1a4268/",
+    icon: LinkedIn,
+  },
+  { label: "Email", href: "mailto:fcarreras777@gmail.com", icon: Mail },
+];
