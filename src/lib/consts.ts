bd60aeedd21a3f1cab07f6b3e34d90609a46aca1
Franco Carreras <fcarreras777@gmail.com>
2025-11-19 23:53:00 -0300

    fix: correct labels for GitHub and LinkedIn in networks export

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -34,9 +34,9 @@ import Services from "@assets/images/skills/services.jpg";
 import Design from "@assets/images/skills/design.jpg";
 
 export const networks = [
-  { label: "GitHub Profil", href: "https://github.com/Fr4nco77", icon: Github },
+  { label: "GitHub", href: "https://github.com/Fr4nco77", icon: Github },
   {
-    label: "LinkedIn Profil",
+    label: "LinkedIn",
     href: "https://www.linkedin.com/in/franco-carreras-b7a1a4268/",
     icon: LinkedIn,
   },
