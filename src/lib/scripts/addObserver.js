807dfa025e0a0ddd4a44a58979d676b6761d1189
Franco Carreras <fcarreras777@gmail.com>
2025-11-21 17:43:02 -0300

    add: centralize entry transition observer in separate script and used in pages

---
+++ src/lib/scripts/addObserver.js
@@ -0,0 +1,25 @@
+//El fin de este script es centralizar un observer necesario
+//para las transiciones de entrada en los componentes que los requieran
+
+(() => {
+  const elements = document.querySelectorAll(".observe-enter");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (!entry.isIntersecting) return;
+
+        const el = entry.target;
+        el.classList.add("animate-fadeIn");
+        observer.unobserve(el);
+      });
+    },
+    {
+      root: null,
+      threshold: 0.2,
+      rootMargin: "10% 0px -10% 0px",
+    },
+  );
+
+  elements.forEach((e) => observer.observe(e));
+})();
