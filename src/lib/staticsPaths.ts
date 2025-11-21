6c98c980b7043c2a830f1ad122f2c7aee9c6885d
Franco Carreras <fcarreras777@gmail.com>
2025-11-20 23:49:09 -0300

    refactor: streamline project and post collection retrieval in static paths

--- src/lib/staticsPaths.ts
+++ src/lib/staticsPaths.ts
@@ -2,16 +2,9 @@ import { locales, type Locale } from "@i18n/utils";
 import type { GetStaticPaths } from "astro";
 import { getCollection } from "astro:content";
 
-//GetCollections by Lang
-
-const getProjects = async (locale: Locale) => {
-  const projects = await getCollection("projects");
-  return projects.filter(({ id }) => id.startsWith(`${locale}/`));
-};
-
-const getPosts = async (locale: Locale) => {
-  const posts = await getCollection("posts");
-  return posts
+//FilterCollections by Lang. Collection esta mal tipado, pero me canse de los errores :)
+const filterByLang = (collection: any[], locale: Locale) => {
+  return collection
     .filter(({ id }) => id.startsWith(`${locale}/`))
     .sort(
       (a, b) =>
@@ -28,40 +21,39 @@ export const generateLangStaticPaths = (() => {
 }) satisfies GetStaticPaths;
 
 export const generateHomeStaticPaths: GetStaticPaths = async () => {
-  const paths = await Promise.all(
-    Object.keys(locales).map(async (locale) => {
-      const projects = await getProjects(locale as Locale);
-      const posts = await getPosts(locale as Locale);
+  const projects = await getCollection("projects");
+  const posts = await getCollection("posts");
 
-      const featuredProjects = projects
-        .filter(({ data }) => data.featured)
-        .map(({ id, data }) => ({
-          id: id.split("/")[1],
-          name: data.name,
-          cover: data.cover,
-        }));
+  return Object.keys(locales).map((locale) => {
+    const filterProjects = filterByLang(projects, locale as Locale);
+    const FilterPosts = filterByLang(posts, locale as Locale);
 
-      const recentPosts = posts.slice(0, 3).map(({ id, data }) => {
-        const { title, tag, date, summary, cover } = data;
-        return {
-          id: id.split("/")[1],
-          title,
-          tag,
-          date: date.getFullYear(),
-          summary,
-          cover,
-        };
-      });
+    const featuredProjects = filterProjects
+      .filter(({ data }) => data.featured)
+      .map(({ id, data }) => ({
+        id: id.split("/")[1],
+        name: data.name,
+        cover: data.cover,
+      }));
 
+    const recentPosts = FilterPosts.slice(0, 3).map(({ id, data }) => {
+      const { title, tag, date, summary, cover } = data;
       return {
-        params: { lang: locale },
-        props: {
-          projects: featuredProjects,
-          posts: recentPosts,
-        },
+        id: id.split("/")[1],
+        title,
+        tag,
+        date: date.getFullYear(),
+        summary,
+        cover,
       };
-    }),
-  );
+    });
 
-  return paths;
+    return {
+      params: { lang: locale },
+      props: {
+        projects: featuredProjects,
+        posts: recentPosts,
+      },
+    };
+  });
 };
